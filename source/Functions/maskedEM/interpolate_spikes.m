534fca17406271949d9fa407a2a676f27b9ca089
Jordan Sorokin <Jorsorokin@gmail.com>
2017-08-09 23:56:00 -0700

    removed unnecessary resampling

--- source/Functions/maskedEM/interpolate_spikes.m
+++ source/Functions/maskedEM/interpolate_spikes.m
@@ -1,9 +1,10 @@
-function snips = get_aligned_spikes( spikes,sptm,fs,prePts,postPts )
-    % snips = get_aligned_spikes( spikes,sptm,fs,preTime,postTime )
+function snips = interpolate_spikes( spikes,sptm,fs,prePts,postPts )
+    % snips = interpolate_spikes( spikes,sptm,fs,preTime,postTime )
     %
-    % align the voltage waveforms contained in "spikes" according to the 
-    % spike time specified by "sptm". "sptm" does not necessarily refer to 
-    % the peak of the waveform, but could be the center of mass, for instance.
+    % interpolates the voltage waveforms contained in "spikes" according to the 
+    % spike time specified by "sptm" using cubic spline interpolation.
+    % "sptm" does not necessarily refer to the peak of the waveform, 
+    % but could be the center of mass, for instance.
     %
     % preTime and postTime are scalars specifying the amount of data (in samples)
     % to keep before/after the aligned spike. This should be less than or equal to the 
@@ -30,11 +31,7 @@ function snips = get_aligned_spikes( spikes,sptm,fs,prePts,postPts )
     for sp = 1:nSp
         try
             x = linspace( sptm(sp)-prePts,sptm(sp)+postPts-1,totalPts );
-            xx = linspace( x(1),x(end),totalPts*4 );
-            upspikes = csaps( xind,squeeze( spikes(:,sp,:) )',0.3,xx )'; % smooths the interpolant
-            
-            % now down-sample back to original sampling rate & extract pre/post time around the spike
-            snips(:,sp,:) = spline( xx,upspikes',x )';
+            snips(:,sp,:) = csaps( xind,squeeze( spikes(:,sp,:) )',0.3,x )'; % smooths the interpolant
         catch
         end
     end
